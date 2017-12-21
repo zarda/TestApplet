21dca460e3da373b50ac3fb40fa2340b7b56918e
zarda <zarda0@hotmail.com>
2017-12-21 17:04:15 +0800

    Regular update

--- ConsoleApp1_siso/ConsoleApp1_siso/Gbe_Simple.Designer.cs
+++ ConsoleApp1_siso/ConsoleApp1_siso/Gbe_Simple.Designer.cs
@@ -33,18 +33,19 @@
             this.label_connect = new System.Windows.Forms.Label();
             this.timer1000 = new System.Windows.Forms.Timer(this.components);
             this.button_Disconnect = new System.Windows.Forms.Button();
-            this.button_connect = new System.Windows.Forms.Button();
+            this.button_Connect = new System.Windows.Forms.Button();
             this.button_DoWork = new System.Windows.Forms.Button();
+            this.hWindowControl_color = new HalconDotNet.HWindowControl();
+            this.hWindowControl_R = new HalconDotNet.HWindowControl();
+            this.hWindowControl_G = new HalconDotNet.HWindowControl();
+            this.hWindowControl_B = new HalconDotNet.HWindowControl();
             this.SuspendLayout();
             // 
             // richTextBox
             // 
-            this.richTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.richTextBox.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox.Location = new System.Drawing.Point(12, 147);
             this.richTextBox.Name = "richTextBox";
-            this.richTextBox.Size = new System.Drawing.Size(311, 238);
+            this.richTextBox.Size = new System.Drawing.Size(234, 321);
             this.richTextBox.TabIndex = 0;
             this.richTextBox.Text = "";
             // 
@@ -52,7 +53,7 @@
             // 
             this.label_connect.AutoSize = true;
             this.label_connect.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label_connect.Location = new System.Drawing.Point(335, 12);
+            this.label_connect.Location = new System.Drawing.Point(8, 6);
             this.label_connect.Name = "label_connect";
             this.label_connect.Size = new System.Drawing.Size(76, 21);
             this.label_connect.TabIndex = 1;
@@ -65,47 +66,97 @@
             // 
             // button_Disconnect
             // 
-            this.button_Disconnect.Location = new System.Drawing.Point(339, 209);
+            this.button_Disconnect.Location = new System.Drawing.Point(12, 105);
             this.button_Disconnect.Name = "button_Disconnect";
-            this.button_Disconnect.Size = new System.Drawing.Size(95, 30);
+            this.button_Disconnect.Size = new System.Drawing.Size(95, 36);
             this.button_Disconnect.TabIndex = 2;
             this.button_Disconnect.Text = "Disconnect";
             this.button_Disconnect.UseVisualStyleBackColor = true;
             this.button_Disconnect.Click += new System.EventHandler(this.button_Disconnect_Click);
             // 
-            // button_connect
+            // button_Connect
             // 
-            this.button_connect.Location = new System.Drawing.Point(339, 173);
-            this.button_connect.Name = "button_connect";
-            this.button_connect.Size = new System.Drawing.Size(95, 30);
-            this.button_connect.TabIndex = 2;
-            this.button_connect.Text = "Connect";
-            this.button_connect.UseVisualStyleBackColor = true;
-            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            this.button_Connect.Location = new System.Drawing.Point(12, 69);
+            this.button_Connect.Name = "button_Connect";
+            this.button_Connect.Size = new System.Drawing.Size(95, 36);
+            this.button_Connect.TabIndex = 2;
+            this.button_Connect.Text = "Connect";
+            this.button_Connect.UseVisualStyleBackColor = true;
+            this.button_Connect.Click += new System.EventHandler(this.button_connect_Click);
             // 
             // button_DoWork
             // 
-            this.button_DoWork.Location = new System.Drawing.Point(339, 137);
+            this.button_DoWork.Location = new System.Drawing.Point(12, 33);
             this.button_DoWork.Name = "button_DoWork";
-            this.button_DoWork.Size = new System.Drawing.Size(95, 30);
+            this.button_DoWork.Size = new System.Drawing.Size(95, 36);
             this.button_DoWork.TabIndex = 2;
             this.button_DoWork.Text = "Run";
             this.button_DoWork.UseVisualStyleBackColor = true;
             this.button_DoWork.Click += new System.EventHandler(this.button_DoWork_Click);
             // 
+            // hWindowControl_color
+            // 
+            this.hWindowControl_color.BackColor = System.Drawing.Color.Black;
+            this.hWindowControl_color.BorderColor = System.Drawing.Color.Black;
+            this.hWindowControl_color.ImagePart = new System.Drawing.Rectangle(0, 0, 640, 480);
+            this.hWindowControl_color.Location = new System.Drawing.Point(252, 6);
+            this.hWindowControl_color.Name = "hWindowControl_color";
+            this.hWindowControl_color.Size = new System.Drawing.Size(460, 460);
+            this.hWindowControl_color.TabIndex = 3;
+            this.hWindowControl_color.WindowSize = new System.Drawing.Size(460, 460);
+            // 
+            // hWindowControl_R
+            // 
+            this.hWindowControl_R.BackColor = System.Drawing.Color.Black;
+            this.hWindowControl_R.BorderColor = System.Drawing.Color.Black;
+            this.hWindowControl_R.ImagePart = new System.Drawing.Rectangle(0, 0, 640, 480);
+            this.hWindowControl_R.Location = new System.Drawing.Point(718, 6);
+            this.hWindowControl_R.Name = "hWindowControl_R";
+            this.hWindowControl_R.Size = new System.Drawing.Size(150, 150);
+            this.hWindowControl_R.TabIndex = 4;
+            this.hWindowControl_R.WindowSize = new System.Drawing.Size(150, 150);
+            // 
+            // hWindowControl_green
+            // 
+            this.hWindowControl_G.BackColor = System.Drawing.Color.Black;
+            this.hWindowControl_G.BorderColor = System.Drawing.Color.Black;
+            this.hWindowControl_G.ImagePart = new System.Drawing.Rectangle(0, 0, 640, 480);
+            this.hWindowControl_G.Location = new System.Drawing.Point(718, 162);
+            this.hWindowControl_G.Name = "hWindowControl_green";
+            this.hWindowControl_G.Size = new System.Drawing.Size(150, 150);
+            this.hWindowControl_G.TabIndex = 4;
+            this.hWindowControl_G.WindowSize = new System.Drawing.Size(150, 150);
+            // 
+            // hWindowControl_blue
+            // 
+            this.hWindowControl_B.BackColor = System.Drawing.Color.Black;
+            this.hWindowControl_B.BorderColor = System.Drawing.Color.Black;
+            this.hWindowControl_B.ImagePart = new System.Drawing.Rectangle(0, 0, 640, 480);
+            this.hWindowControl_B.Location = new System.Drawing.Point(718, 318);
+            this.hWindowControl_B.Name = "hWindowControl_blue";
+            this.hWindowControl_B.Size = new System.Drawing.Size(150, 150);
+            this.hWindowControl_B.TabIndex = 4;
+            this.hWindowControl_B.WindowSize = new System.Drawing.Size(150, 150);
+            // 
             // Gbe_Simple
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(455, 262);
+            this.ClientSize = new System.Drawing.Size(877, 478);
+            this.Controls.Add(this.hWindowControl_B);
+            this.Controls.Add(this.hWindowControl_G);
+            this.Controls.Add(this.hWindowControl_R);
+            this.Controls.Add(this.hWindowControl_color);
             this.Controls.Add(this.button_DoWork);
-            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.button_Connect);
             this.Controls.Add(this.button_Disconnect);
             this.Controls.Add(this.label_connect);
             this.Controls.Add(this.richTextBox);
+            this.MinimumSize = new System.Drawing.Size(893, 516);
             this.Name = "Gbe_Simple";
             this.Text = "Gbe_Simple";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Gbe_Simple_FormClosed);
+            this.ResizeEnd += new System.EventHandler(this.Gbe_Simple_ResizeEnd);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,7 +168,11 @@
         private System.Windows.Forms.Label label_connect;
         private System.Windows.Forms.Timer timer1000;
         private System.Windows.Forms.Button button_Disconnect;
-        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.Button button_Connect;
         private System.Windows.Forms.Button button_DoWork;
+        private HalconDotNet.HWindowControl hWindowControl_color;
+        private HalconDotNet.HWindowControl hWindowControl_R;
+        private HalconDotNet.HWindowControl hWindowControl_G;
+        private HalconDotNet.HWindowControl hWindowControl_B;
     }
 }
--- ConsoleApp1_siso/ConsoleApp1_siso/Gbe_Simple.cs
+++ ConsoleApp1_siso/ConsoleApp1_siso/Gbe_Simple.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -15,21 +16,34 @@ namespace ConsoleApp1_siso
     {
         public Gbe_Simple()
         {
-            InitializeComponent();   
+            InitializeComponent();
 
+            button_Connect.Enabled = true;
+            button_Disconnect.Enabled = false;
+            button_DoWork.Enabled = false;
         }
 
+        private Fg_Struct fg = null;
         private BoardHandle board = null;
         //private int CameraCount = 0;
         private CameraHandle cameraHandle = null;
         private int error = 0;
+        private uint camPort = (uint)SiSoCsRt.PORT_A;
+        
+
         private void DeviceStart()
         {
+            fg = SiSoCsRt.Fg_Init("QuadAreaBayer24", 0);
+            if (fg == null)
+            {
+                throw new Exception( "Fg_Init : " + SiSoCsRt.Fg_getLastErrorDescription(null)); 
+            }
+
             board = SiSoCsRt.Gbe_initBoard(0, 0, out error);
-            SisoErrorThrow(error, "Gbe_initBoard");
+            SisoGbeErrorThrow(error, "Gbe_initBoard");
 
             error = SiSoCsRt.Gbe_scanNetwork(board, -1, 200);
-            SisoErrorThrow(error, "Gbe_scanNetwork");
+            SisoGbeErrorThrow(error, "Gbe_scanNetwork");
 
             //for (int i = 0; i < 4; i++)
             //{
@@ -38,22 +52,29 @@ namespace ConsoleApp1_siso
             //    richTextBox.Text += $"Port {i} Camera Count = {CameraCount} \n"; 
             //}
 
-            cameraHandle = SiSoCsRt.Gbe_getFirstCamera(board, 0, out error);
-            SisoErrorThrow(error, "Gbe_getFirstCamera");
+            cameraHandle = SiSoCsRt.Gbe_getFirstCamera(board, (int)camPort, out error);
+            SisoGbeErrorThrow(error, "Gbe_getFirstCamera");
 
         }
-        private void SisoErrorThrow(int error, string command)
+        private void SisoGbeErrorThrow(int error, string command)
         {
             if (error < 0)
             {
                 throw new Exception(command + " : " + SiSoCsRt.Gbe_getErrorDescription(error));
             }
         }
+        private void SisoFgErrorThrow(int error, string command)
+        {
+            if (error < 0)
+            {
+                throw new Exception(command + " : " + SiSoCsRt.Fg_getErrorDescription(error));
+            }
+        }
 
         private void GetInformation()
         {
             var camInfo = SiSoCsRt.Gbe_getCameraInfo(cameraHandle);
-            SisoErrorThrow(error, "Gbe_getCameraInfo");
+            SisoGbeErrorThrow(error, "Gbe_getCameraInfo");
             richTextBox.Text += $"Camera Vendor Name = {camInfo.manufactor_name} \n";
             richTextBox.Text += $"Camera IP = {camInfo.ipv4} \n";
             richTextBox.Text += $"Camera User Defined Name = {camInfo.user_name} \n";
@@ -63,67 +84,279 @@ namespace ConsoleApp1_siso
             richTextBox.Text += $"Camera Gain Range = {GetGainRange().minimum} ~ {GetGainRange().maximum} \n";
             richTextBox.Text += $"Camera Gain = {GetGain()} \n";
 
+            richTextBox.Text += $"Camera ExposureTime Range = {GetExposureTimeRange().minimum} ~ {GetExposureTimeRange().maximum} \n";
+            richTextBox.Text += $"Camera ExposureTime = {GetExposureTime()} \n";
+
+            richTextBox.Text += $"Camera Width Range = {GetWidthRange().minimum} ~ {GetWidthRange().maximum} by {GetWidthRange().increase} \n";
+            richTextBox.Text += $"Camera Width = {GetWidth()} \n";
+
+            richTextBox.Text += $"Camera Height Range = {GetHeightRange().minimum} ~ {GetHeightRange().maximum} by {GetHeightRange().increase} \n";
+            richTextBox.Text += $"Camera Height = {GetHeight()} \n";
+
+            richTextBox.Text += $"Camera OffsetX Range = {GetOffsetXRange().minimum} ~ {GetOffsetXRange().maximum} by {GetOffsetXRange().increase} \n";
+            richTextBox.Text += $"Camera OffsetX = {GetOffsetX()} \n";
+
+            richTextBox.Text += $"Camera OffsetY Range = {GetOffsetYRange().minimum} ~ {GetOffsetYRange().maximum} by {GetOffsetYRange().increase} \n";
+            richTextBox.Text += $"Camera OffsetY = {GetOffsetY()} \n";
         }
 
         private (double maximum, double minimum) GetGainRange()
         {
             double Min = 0, Max = 0;
             error = SiSoCsRt.Gbe_getFloatValueLimits(cameraHandle, "Gain", ref Min, ref Max);
-            SisoErrorThrow(error, "Gbe_stopAcquisition");
+            SisoGbeErrorThrow(error, "Gbe_getFloatValueLimits(Gain)");
             return (Max, Min);
         }
         private double GetGain()
         {
             double value = 0;
             error = SiSoCsRt.Gbe_getFloatValue(cameraHandle, "Gain", ref value);
-            SisoErrorThrow(error, "Gbe_stopAcquisition");
+            SisoGbeErrorThrow(error, "Gbe_getFloatValue(Gain)");
+            return value;
+        }
+        private double SetGain(double value)
+        {
+            value = GetGainRange().minimum < value ? value < GetGainRange().maximum ? value : GetGainRange().maximum :GetGainRange().minimum;
+            error = SiSoCsRt.Gbe_setFloatValue(cameraHandle, "Gain", value);
+            SisoGbeErrorThrow(error, "Gbe_getFloatValue(Gain)");
+            return value;
+        }
+
+        private (double maximum, double minimum) GetExposureTimeRange()
+        {
+            double Min = 0, Max = 0;
+            error = SiSoCsRt.Gbe_getFloatValueLimits(cameraHandle, "ExposureTime", ref Min, ref Max);
+            SisoGbeErrorThrow(error, "Gbe_getFloatValueLimits(ExposureTime)");
+            return (Max, Min);
+        }
+        private double GetExposureTime()
+        {
+            double value = 0;
+            error = SiSoCsRt.Gbe_getFloatValue(cameraHandle, "ExposureTime", ref value);
+            SisoGbeErrorThrow(error, "Gbe_getFloatValue(ExposureTime)");
+            return value;
+        }
+        private double SetExposureTime(double value)
+        {
+            value = GetExposureTimeRange().minimum < value ? value < GetExposureTimeRange().maximum ? value : GetExposureTimeRange().maximum : GetExposureTimeRange().minimum;
+            error = SiSoCsRt.Gbe_setFloatValue(cameraHandle, "ExposureTime", value);
+            SisoGbeErrorThrow(error, "Gbe_getFloatValue(ExposureTime)");
+            return value;
+        }
+
+        private (long maximum, long minimum, long increase) GetWidthRange()
+        {
+            long Min = 0, Max = 0, Inc = 0;
+            error = SiSoCsRt.Gbe_getIntegerValueLimits(cameraHandle, "Width", ref Min, ref Max, ref Inc);
+            SisoGbeErrorThrow(error, "Gbe_getIntegerValueLimits(Width)");
+            return (Max, Min, Inc);
+        }
+        private long GetWidth()
+        {
+            long value = 0;
+            error = SiSoCsRt.Gbe_getIntegerValue(cameraHandle, "Width", ref value);
+            SisoGbeErrorThrow(error, "Gbe_getIntegerValue(Width)");
+            return value;
+        }
+        private long SetWidth(long value)
+        {
+            value -= value % GetWidthRange().increase;
+            value = GetWidthRange().minimum < value ? value < GetWidthRange().maximum ? value : GetWidthRange().maximum : GetWidthRange().minimum;
+            error = SiSoCsRt.Gbe_setIntegerValue(cameraHandle, "Width", value);
+            SisoGbeErrorThrow(error, "Gbe_getIntegerValue(Width)");
+            return value;
+        }
+
+        private (long maximum, long minimum, long increase) GetHeightRange()
+        {
+            long Min = 0, Max = 0, Inc=0;
+            error = SiSoCsRt.Gbe_getIntegerValueLimits(cameraHandle, "Height", ref Min, ref Max, ref Inc);
+            SisoGbeErrorThrow(error, "Gbe_getFloatValueLimits(Height)");
+            return (Max, Min, Inc);
+        }
+        private long GetHeight()
+        {
+            long value = 0;
+            error = SiSoCsRt.Gbe_getIntegerValue(cameraHandle, "Height", ref value);
+            SisoGbeErrorThrow(error, "Gbe_getIntegerValue(Height)");
+            return value;
+        }
+        private long SetHeight(long value)
+        {
+            value -= value % GetHeightRange().increase;
+            value = GetHeightRange().minimum < value ? value < GetHeightRange().maximum ? value : GetHeightRange().maximum : GetHeightRange().minimum;
+            error = SiSoCsRt.Gbe_setIntegerValue(cameraHandle, "Height", value);
+            SisoGbeErrorThrow(error, "Gbe_getIntegerValue(Height)");
+            return value;
+        }
+
+        private (long maximum, long minimum, long increase) GetOffsetXRange()
+        {
+            long Min = 0, Max = 0, Inc = 0;
+            error = SiSoCsRt.Gbe_getIntegerValueLimits(cameraHandle, "OffsetX", ref Min, ref Max, ref Inc);
+            SisoGbeErrorThrow(error, "Gbe_getFloatValueLimits(OffsetX)");
+            return (Max, Min, Inc);
+        }
+        private long GetOffsetX()
+        {
+            long value = 0;
+            error = SiSoCsRt.Gbe_getIntegerValue(cameraHandle, "OffsetX", ref value);
+            SisoGbeErrorThrow(error, "Gbe_getIntegerValue(OffsetX)");
+            return value;
+        }
+        private long SetOffsetX(long value)
+        {
+            value -= value % GetOffsetXRange().increase;
+            value = GetOffsetXRange().minimum < value ? value < GetOffsetXRange().maximum ? value : GetOffsetXRange().maximum : GetOffsetXRange().minimum;
+            error = SiSoCsRt.Gbe_setIntegerValue(cameraHandle, "OffsetX", value);
+            SisoGbeErrorThrow(error, "Gbe_getIntegerValue(OffsetX)");
+            return value;
+        }
+
+        private (long maximum, long minimum, long increase) GetOffsetYRange()
+        {
+            long Min = 0, Max = 0, Inc = 0;
+            error = SiSoCsRt.Gbe_getIntegerValueLimits(cameraHandle, "OffsetY", ref Min, ref Max, ref Inc);
+            SisoGbeErrorThrow(error, "Gbe_getFloatValueLimits(OffsetY)");
+            return (Max, Min, Inc);
+        }
+        private long GetOffsetY()
+        {
+            long value = 0;
+            error = SiSoCsRt.Gbe_getIntegerValue(cameraHandle, "OffsetY", ref value);
+            SisoGbeErrorThrow(error, "Gbe_getIntegerValue(OffsetY)");
+            return value;
+        }
+        private long SetOffsetY(long value)
+        {
+            value -= value % GetOffsetYRange().increase;
+            value = GetOffsetYRange().minimum < value ? value < GetOffsetYRange().maximum ? value : GetOffsetYRange().maximum : GetOffsetYRange().minimum;
+            error = SiSoCsRt.Gbe_setIntegerValue(cameraHandle, "OffsetY", value);
+            SisoGbeErrorThrow(error, "Gbe_getIntegerValue(OffsetY)");
             return value;
         }
 
         private void DoWork()
         {
-            StartAcquisition();
-            //Thread.Sleep(500);
             
+            TestGrab();
 
-            StopAcquisition();
-            SoftwareTrigger();
+        }
+
+        private void TestGrab()
+        {
+            int nrOfPicturesToGrab = 60;
+            int nbBuffers = 5;
+            uint width  = (uint)GetWidth();
+            uint height = (uint)GetHeight();
+            int samplePerPixel = 1;
+            uint bytePerSample = 3;
+
+            uint imageSize = (uint)(bytePerSample * samplePerPixel * width * height);
+
+            // Create Display
+            //int dispId0 = SiSoCsRt.CreateDisplay((uint)(8 * bytePerSample * samplePerPixel), width, height);
+            //SiSoCsRt.SetBufferWidth(dispId0, width, height);
+
+            // AllocateDMA
+            uint totalBufferSize = (uint)(width * height * samplePerPixel * bytePerSample * nbBuffers);
+            dma_mem memHandle = SiSoCsRt.Fg_AllocMemEx(fg, totalBufferSize, nbBuffers);
+
+            //var imageBuffer = SiSoCsRt.Fg_AllocMem(fg, totalBufferSize, 1, (uint)camPort);
+
+            error = SiSoCsRt.Fg_setParameterWithUInt(fg, SiSoCsRt.FG_WIDTH, width, camPort);
+            SisoFgErrorThrow(error, "Fg_setParameterWithUInt(FG_WIDTH)");
+            error = SiSoCsRt.Fg_setParameterWithUInt(fg, SiSoCsRt.FG_HEIGHT, height, camPort);
+            SisoFgErrorThrow(error, "Fg_setParameterWithUInt(FG_HEIGHT)");
+            SiSoCsRt.Fg_setParameterWithInt(fg, SiSoCsRt.FG_BITALIGNMENT, SiSoCsRt.FG_LEFT_ALIGNED, camPort);
+            SisoFgErrorThrow(error, "Fg_setParameterWithInt(FG_BITALIGNMENT)");
+
+            error = SiSoCsRt.Fg_AcquireEx(fg, camPort, nrOfPicturesToGrab, SiSoCsRt.ACQ_STANDARD, memHandle);
+            SisoFgErrorThrow(error, "Fg_AcquireEx");
+
+            StartGbeAcquisition();
+
+            int last_pic_nr = 0;
+            int cur_pic_nr = 0;
+            while ((cur_pic_nr = SiSoCsRt.Fg_getLastPicNumberBlockingEx(fg, last_pic_nr + 1, camPort, 100, memHandle)) < nrOfPicturesToGrab)
+            //while ((cur_pic_nr = SiSoCsRt.Fg_getLastPicNumberEx(fg, camPort, memHandle)) < nrOfPicturesToGrab)
+            {
+                last_pic_nr = cur_pic_nr;
+                //SiSoCsRt.DrawBuffer(dispId0, SiSoCsRt.Fg_getImagePtrEx(fg, last_pic_nr, (uint)camPort, memHandle), last_pic_nr, "");
+
+                SisoImage img = SiSoCsRt.Fg_getImagePtrEx(fg, cur_pic_nr, camPort, memHandle);
+                using (var hImage = new HalconDotNet.HImage())
+                {
+                    hImage.GenImageInterleaved(img.asPtr(), "bgr", (int)width, (int)height, 0, "byte", 0, 0, 0, 0, -1, 0);
+
+                    hWindowControl_color.SetFullImagePart(hImage);
+                    hImage.DispColor(hWindowControl_color.HalconWindow);
+
+                    using (var hImageTemp1 = hImage.Decompose3(out var hImageTemp2, out var hImageTemp3))
+                    {
+                        hWindowControl_R.SetFullImagePart(hImageTemp1);
+                        hWindowControl_G.SetFullImagePart(hImageTemp2);
+                        hWindowControl_B.SetFullImagePart(hImageTemp3);
+                        hImageTemp1.DispImage(hWindowControl_R.HalconWindow);
+                        hImageTemp2.DispImage(hWindowControl_G.HalconWindow);
+                        hImageTemp3.DispImage(hWindowControl_B.HalconWindow);
+                    }
+                }
+                if (cur_pic_nr % nbBuffers == 0)
+                {
+                    GC.Collect();
+                }
+            }
+
+            error = SiSoCsRt.Fg_stopAcquireEx(fg, camPort, memHandle, SiSoCsRt.FG_STOP_TIMEOUT);
+            SisoFgErrorThrow(error, "Fg_stopAcquireEx");
+
+            StopGbeAcquisition();
+
+            //error = SiSoCsRt.Fg_FreeMem(fg, (uint)camPort);
+            //SisoFgErrorThrow(error, "Fg_FreeMem");
+
+            error = SiSoCsRt.Fg_FreeMemEx(fg, memHandle);
+            SisoFgErrorThrow(error, "Fg_FreeMemEx");
         }
 
         private void SoftwareTrigger()
         {
             error = SiSoCsRt.Gbe_setEnumerationValue(cameraHandle, "TriggerMode", "On");
-            SisoErrorThrow(error, "Gbe_setEnumerationValue(TriggerMode:On)");
+            SisoGbeErrorThrow(error, "Gbe_setEnumerationValue(TriggerMode:On)");
             error = SiSoCsRt.Gbe_setEnumerationValue(cameraHandle, "TriggerSource", "Software");
-            SisoErrorThrow(error, "Gbe_setEnumerationValue(TriggerSource:Software)");
+            SisoGbeErrorThrow(error, "Gbe_setEnumerationValue(TriggerSource:Software)");
             error = SiSoCsRt.Gbe_executeCommand(cameraHandle, "TriggerSoftware");
-            SisoErrorThrow(error, "Gbe_executeCommand(TriggerSoftware)");
+            SisoGbeErrorThrow(error, "Gbe_executeCommand(TriggerSoftware)");
             error = SiSoCsRt.Gbe_setEnumerationValue(cameraHandle, "TriggerMode", "Off");
-            SisoErrorThrow(error, "Gbe_setEnumerationValue(TriggerMode:Off)");
+            SisoGbeErrorThrow(error, "Gbe_setEnumerationValue(TriggerMode:Off)");
         }
 
-        private void StopAcquisition()
+        private void StopGbeAcquisition()
         {
             error = SiSoCsRt.Gbe_stopAcquisition(cameraHandle);
-            SisoErrorThrow(error, "Gbe_stopAcquisition");
+            SisoGbeErrorThrow(error, "Gbe_stopAcquisition");
         }
 
-        private void StartAcquisition()
+        private void StartGbeAcquisition()
         {
             error = SiSoCsRt.Gbe_startAcquisition(cameraHandle);
-            SisoErrorThrow(error, "Gbe_startAcquisition");
+            SisoGbeErrorThrow(error, "Gbe_startAcquisition");
         }
 
         private void DisconnectDevice()
         {
-            error = SiSoCsRt.Gbe_disconnectCamera(cameraHandle);
-            SisoErrorThrow(error, "Gbe_disconnectCamera");
+            if (cameraHandle != null)
+            {
+                error = SiSoCsRt.Gbe_disconnectCamera(cameraHandle);
+                SisoGbeErrorThrow(error, "Gbe_disconnectCamera"); 
+            }
         }
 
         private void ConnectDevice()
         {
             error = SiSoCsRt.Gbe_connectCamera(cameraHandle);
-            SisoErrorThrow(error, "Gbe_connectCamera");
+            SisoGbeErrorThrow(error, "Gbe_connectCamera");
         }
 
         private void DeviceOff()
@@ -136,6 +369,10 @@ namespace ConsoleApp1_siso
             {
                 SiSoCsRt.Gbe_freeBoard(board);
             }
+            if (fg != null)
+            {
+                SiSoCsRt.Fg_FreeGrabber(fg);
+            }
         }
 
         private void timer1000_Tick(object sender, EventArgs e)
@@ -144,11 +381,17 @@ namespace ConsoleApp1_siso
             {
                 label_connect.BackColor = Color.Green;
                 label_connect.Text = "Connected";
+                button_Connect.Enabled = false;
+                button_Disconnect.Enabled = true;
+                button_DoWork.Enabled = true;
             }
             else
             {
                 label_connect.BackColor = Color.Gray;
                 label_connect.Text = "Disconnected";
+                button_Connect.Enabled = true;
+                button_Disconnect.Enabled = false;
+                button_DoWork.Enabled = false;
             }
         }
 
@@ -178,6 +421,11 @@ namespace ConsoleApp1_siso
             {
                 button_Disconnect_Click(this, e);
             }
+        }
+
+        private void Gbe_Simple_ResizeEnd(object sender, EventArgs e)
+        {
+            hWindowControl_color.Refresh();
         }
     }
 }
