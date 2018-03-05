a575e3f0970efdcba198613cc82c20d64c5df9ac
zarda <zarda0@hotmail.com>
2018-03-05 09:53:02 +0800

    Add new test demo

---
+++ Rex_WordFilter/Rex_WordFilter/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Rex_WordFilter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(20, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 27);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox2.Location = new System.Drawing.Point(20, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(237, 27);
+            this.textBox2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Rex_WordFilter/Rex_WordFilter/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rex_WordFilter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            /// XML ocupied
+            List<string> forbidden = new List<string>();
+            forbidden.Add(" ");
+            forbidden.Add("<");
+            forbidden.Add(">");
+            forbidden.Add("&");
+            forbidden.Add("\'");
+            forbidden.Add("\"");
+            forbidden.Add("\\");
+            forbidden.Add("/");
+            /// Numbers
+            List<string> number = new List<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                number.Add(i.ToString());
+            }
+            //textBox2.Text = VerifyWord1(textBox1.Text, " ").ToString();
+            //textBox2.Text = VerifyWord2(textBox1.Text, " ").ToString();
+            //textBox2.Text = VerifyWord3(textBox1.Text, " ").ToString();
+            //textBox2.Text = WordContain(textBox1.Text, forbidden).ToString();
+            //textBox2.Text = WordStartWith(textBox1.Text, forbidden).ToString();
+            button1.Enabled = !(WordStartWith(textBox1.Text, number)| WordContain(textBox1.Text, forbidden));
+        }
+
+        private bool WordStartWith(string text, List<string> forbidden)
+        {
+            bool result = false;
+            foreach (var check in forbidden)
+            {
+                result |= text.StartsWith(check);
+            }
+            return result;
+        }
+
+        private bool WordContain(string text, List<string> forbidden)
+        {
+            bool result = false;
+            foreach (var check in forbidden)
+            {
+                result |= text.Contains(check);
+            }
+            return result;
+        }
+
+        private bool VerifyWord3(string text, string check)
+        {
+            return text.StartsWith(check);
+        }
+
+        private bool VerifyWord2(string text, string check)
+        {
+            Regex Check = new Regex(check);
+            return Check.IsMatch(text);
+        }
+
+        private bool VerifyWord1(string text, string chip)
+        {
+            return text.Contains(chip);
+        }
+
+
+    }
+}
