13e0e217e5b6c4eecebccc11599533fd2f5e3dda
zarda <zarda0@hotmail.com>
2018-03-30 09:04:53 +0800

    Add new test about behavior at FromApp

---
+++ WindowsFormsBehavior/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonError = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonError
+            // 
+            this.buttonError.Location = new System.Drawing.Point(25, 171);
+            this.buttonError.Name = "buttonError";
+            this.buttonError.Size = new System.Drawing.Size(138, 63);
+            this.buttonError.TabIndex = 0;
+            this.buttonError.Text = "Error trigger";
+            this.buttonError.UseVisualStyleBackColor = true;
+            this.buttonError.Click += new System.EventHandler(this.buttonError_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.buttonError);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonError;
+    }
+}
+
---
+++ WindowsFormsBehavior/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var fileName = "D:\\test.txt";
+            // Check if file exist.
+            if (!System.IO.File.Exists(fileName))
+            {
+                // Write an array of strings to a file.
+                // Create a string array that consists of three lines.
+                string[] lines = { "Form closing log:" };
+                // WriteAllLines creates a file, writes a collection of strings to the file,
+                // and then closes the file.  You do NOT need to call Flush() or Close().
+                System.IO.File.WriteAllLines(fileName, lines);
+            }
+            // Append new text to an existing file.
+            // The using statement automatically flushes AND CLOSES the stream and calls 
+            // IDisposable.Dispose on the stream object.
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(fileName, true))
+            {
+                file.WriteLine($"Form shutdown at {DateTime.Now}");
+            }
+
+        }
+
+        private void buttonError_Click(object sender, EventArgs e)
+        {
+            var zero = 0;
+            var tt = 1 / zero;
+        }
+    }
+}
